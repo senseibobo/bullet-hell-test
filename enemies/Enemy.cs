5d65b3eea8674f464189e0776744b4d282144f15
bogi000 <bogdanjsd@gmail.com>
2021-11-12 20:37:44 +0100

    added phases to enemies

--- enemies/Enemy.cs
+++ enemies/Enemy.cs
@@ -4,6 +4,7 @@ using System;
 public class Enemy : Character
 {
     [Export] public float radius = 16.0f;
+    protected int phase = 1;
     public override void _Ready()
     {
         base._Ready();
